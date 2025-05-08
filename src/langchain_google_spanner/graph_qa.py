fc2e72ff77e454b6fc8340f6f48ef9b8ba3b8b7c
Jordi Mas <jmas@softcatala.org>
2025-05-08 07:32:09 +0200

    Move raw_generated_gql just before verified_gql

--- src/langchain_google_spanner/graph_qa.py
+++ src/langchain_google_spanner/graph_qa.py
@@ -301,7 +301,6 @@ class SpannerGraphQAChain(Chain):
         generated_gql = extract_gql(gen_response)
 
         if self.verify_gql:
-            intermediate_steps.append({"raw_generated_gql": generated_gql})
             verify_response = self.gql_verify_chain.invoke(
                 {
                     "question": question,
@@ -310,6 +309,7 @@ class SpannerGraphQAChain(Chain):
                 }
             )
             if "verified_gql" in verify_response:
+                intermediate_steps.append({"raw_generated_gql": generated_gql})
                 verified_gql = fix_gql_syntax(verify_response["verified_gql"])
                 intermediate_steps.append({"verified_gql": verified_gql})
             else:
